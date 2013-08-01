dab46670691683a734f404deb334f55a6a3b1e62
Nate Brennand <natebrennand@gmail.com>
2013-08-01 15:35:32 -0400

    make setup script more concise

--- setup.sh
+++ setup.sh
@@ -6,14 +6,12 @@ mv  ~/.gitignore \
     ~/.gitconfig \
     ~/.pythonrc \
     ~/.vimrc \
+    ~/.vim \
     ~/.zshrc \
         ~/dotfile_backup
 
-ln .gitignore   ~/.gitignore
-ln .gitconfig   ~/.gitconfig
-ln .pythonrc    ~/.pythonrc
-ln .vimrc       ~/.vimrc
-ln .zshrc       ~/.zshrc
+ln -s dotfiles/.*   ~
+ln -s .vim ~
 
 echo "Your old dotfiles are saved in ~/dotfile_backup"
 
