54d9f801e1886e623363e17237f13bbc3113a82f
Nate Brennand <natebrennand@gmail.com>
2013-09-30 22:00:49 -0400

    updates and reconfiguration

--- scripts/digital_ocean.sh
+++ scripts/digital_ocean.sh
@@ -7,10 +7,10 @@ apt-get -y install vim
 
 # zsh stuff
 apt-get -y install zsh git-core
+apt-get -y install zsh git
+apt-get install zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 chsh -s `which zsh`
 
-# set up my dotfiles
-bash setup.sh
 
