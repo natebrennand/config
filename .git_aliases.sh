324e12f3364e39716013a4643fb7d27c079ebf72
Nate Brennand <natebrennand@gmail.com>
2014-08-13 15:47:39 -0700

    better git aliases

--- .git_aliases.sh
+++ .git_aliases.sh
@@ -1,16 +1,18 @@
 
 alias g='git'
 
-alias gst='git status'
+alias gst='git status -s'
+alias gstv='git status'
 
 alias ga='git add'
-alias gc='git commit'
-alias gc!='git commit -amend'
+alias gc='git commit -v'
+alias gc!='git commit -v --amend'
 
 alias gd='git diff'
 alias gdc='git diff --cached'
 
 alias gl='git pull'
+alias glr='git pull --rebase'
 alias gp='git push'
 
 alias gco='git checkout'
@@ -21,4 +23,11 @@ alias grh='git reset HEAD'
 alias grhh='git reset HEAD --hard'
 
 alias gb='git branch'
+
+alias glo='git log --oneline --decorate --color'
+alias glg='git log --stat --max-count=10'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+alias glgg='git log --graph --max-count=10'
+
+
 
