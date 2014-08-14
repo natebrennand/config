1a59ef28deb686003980f473e70b9181dc679727
Nate Brennand <natebrennand@gmail.com>
2014-08-13 20:55:23 -0700

    alias & PATH overhaul

--- .my_aliases.sh
+++ .my_aliases.sh
@@ -1,16 +1,13 @@
 
 # aliases
 alias zshconfig="vim ~/.zshrc"
-alias mou='open /opt/homebrew-cask/Caskroom/mou/latest/Mou.app'
-alias ll='ls -alF'
 alias l='ls -1h'
-alias less="/usr/share/vim/vim73/macros/less.sh"    # coloring while using less
-alias mtr="sudo mtr"                # ping on steroids
-alias json='python -m json.tool'     # pipe a json to this for pretty print
+alias ll='ls -alF'
+alias less="/usr/share/vim/vim73/macros/less.sh" # coloring while using less
+alias mtr="sudo mtr"                             # ping on steroids
+alias json='python -m json.tool'                 # pipe a json to this for pretty print
 alias server='python -m SimpleHTTPServer'
-alias pipes='/usr/local/Cellar/bash/4.2.45/bin/bash /Users/nate/cloned/pipes/pipes.sh'  # screensaver
 alias latex='/usr/texbin/pdflatex'
-alias ansiweather='/Users/nate/cloned/ansiweather/ansiweather'
 alias vi='vim'
 alias nosetests='nosetests -v'
 
@@ -30,7 +27,4 @@ alias untar='tar -zxvf'
 
 # choo choo
 alias sl='\sl'
-
-# golang
-#alias gget='pushd; go get 
 
