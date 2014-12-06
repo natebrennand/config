53537bc0624140dae0b7750f0366746a2397a54d
Nate Brennand <natebrennand@gmail.com>
2014-12-05 21:12:55 -0500

    winter cleaning?

--- scripts/add_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for dir in ".vim" "scripts" "itermcolors"
-do
-    git add -f $dir
-done
-
-exit 0
-
--- scripts/setup_mongo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-apt-get -y update
-apt-get -y install mongodb
-mkdir -p /data/db/
-
