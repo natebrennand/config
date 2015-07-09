2d442fbc82ebef789bff126341f6fa7e9511d2b1
Nate Brennand <natebrennand@gmail.com>
2015-07-09 14:39:09 -0700

    think it actually works now

--- setup.sh
+++ setup.sh
@@ -11,24 +11,22 @@ ln -s $here/.gitconfig ~/.gitconfig
 ln -s $here/.global_gitignore ~/.global_gitignore
 ln -s $here/.zshrc ~/.zshrc
 ln -s $here/.zpreztorc ~/.zpreztorc
-
-
-cd ~/.vim/bundle/YouCompleteMe
-
-git submodule update --init --recursive
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-    sudo apt-get install -y CMake build-essential python-dev libclang-common-dev
-    sudo add-apt-repository ppa:fcwu-tw/ppa
-    sudo apt-get update
-    sudo apt-get install vim
-fi
-
-./install.sh --clang-completer --system-libclang
+ln -s $here/.my_aliases.sh ~/.my_aliases.sh
+ln -s $here/.git_aliases.sh ~/.git_aliases.sh
 
 vim +BundleInstall +qall
 
 
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install neovim
+git submodule update --init --recursive
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+    sudo apt-get update -y
+    sudo apt-get install -y CMake build-essential python-dev libclang-dev
+fi
+
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.sh --clang-completer --system-libclang
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
