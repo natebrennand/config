d118a5facc506899f67af7894b906252d9103e49
Nate Brennand <natebrennand@gmail.com>
2015-07-09 14:03:01 -0700

    try updating it more...

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 here=$(pwd)
 
 git submodule update --init --recursive
-
 ln -s $here/.vim ~/.vim
 ln -s $here/.vim ~/.nvim
 ln -s $here/.vimrc ~/.vimrc
@@ -13,7 +12,23 @@ ln -s $here/.global_gitignore ~/.global_gitignore
 ln -s $here/.zshrc ~/.zshrc
 ln -s $here/.zpreztorc ~/.zpreztorc
 
-vim .nvimrc -c ':PluginInstall'
-cd .vim/bundle/YouCompleteMe && ./install.sh --clang-completer
+
+cd ~/.vim/bundle/YouCompleteMe
+
+git submodule update --init --recursive
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+    sudo apt-get install -y CMake build-essential python-dev libclang-common-dev
+    sudo add-apt-repository ppa:fcwu-tw/ppa
+    sudo apt-get update
+    sudo apt-get install vim
+fi
+
+./install.sh --clang-completer --system-libclang
+
+vim +BundleInstall +qall
 
 
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
