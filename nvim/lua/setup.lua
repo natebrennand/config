bd29fef59d01f22a8510ddde3f0a77e98327f568
Nate Brennand <nate@metronome.com>
2023-01-11 13:36:42 -0800

    update neovim config away from deprecated function

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -46,7 +46,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
 
 lspconfig['gopls'].setup {
